0ffebb8fceb752c7c0e4b7aa81d2dbea6d677452
benlu <benlu@benlutekiMacBook-Pro.local>
2013-09-08 00:19:17 +0800

    Initial Commit

---
+++ 2storyboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  2storyboard
+//
+//  Created by benlu on 9/8/13.
+//  Copyright (c) 2013 benlu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
